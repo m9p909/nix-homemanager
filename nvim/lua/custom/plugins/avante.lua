2ce78b842a84580d031d246e10de6e5a30a946e9
m9p909 <jackfulcher09@gmail.com>
2025-12-29 20:11:27 -0500

    init nix with neovim

---
+++ nvim/lua/custom/plugins/avante.lua
@@ -0,0 +1,79 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    -- add any opts here
+    -- for example
+    providers = {
+      openrouter = {
+        __inherited_from = 'openai',
+        endpoint = 'https://openrouter.ai/api/v1',
+        api_key_name = 'OPENAI_API_KEY',
+        model = 'anthropic/claude-sonnet-4.5',
+        max_tokens = 8192,
+      },
+    },
+    cursor_applying_provider = 'openrouter',
+    provider = 'openrouter',
+    behaviour = {
+      --- ... existing behaviours
+      enable_cursor_planning_mode = false, -- enable cursor planning mode!
+    },
+    --    system_prompt = function()
+    --      local hub = require('mcphub').get_hub_instance()
+    --      return hub and hub:get_active_servers_prompt() or ''
+    --    end,
+    -- Using function prevents requiring mcphub before it's loaded
+    --    custom_tools = function()
+    --      return {
+    --        require('mcphub.extensions.avante').mcp_tool(),
+    --      }
+    --    end,
+  },
+
+  keys = {
+    { '<leader>ae', '<cmd>AvanteClear<cr>', desc = 'Avante: Clear Chat', silent = true },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,27 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+--
+-- live grep keymap
+--
+--
+
+local map = function(keys, func, desc, mode)
+  mode = mode or 'n'
+  vim.keymap.set(mode, keys, func, { desc = 'LSP: ' .. desc })
+end
+
+map('<leader>df', vim.diagnostic.open_float, 'Describe Float')
+return {
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('refactoring').setup()
+    end,
+  },
+}
