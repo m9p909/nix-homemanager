deaf5acb42363d86dae29869c58dd33f451230e8
m9p909 <jackfulcher09@gmail.com>
2026-07-10 17:30:46 -0400

    add emacs

--- home.nix
+++ home.nix
@@ -94,6 +94,8 @@
     pkgs.ocamlPackages.utop
     pkgs.ocamlPackages.odoc
 
+    pkgs.emacs
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -134,6 +136,10 @@
     ".config/opencode/skills" = {
       source = ./config/opencode/skills;
       recursive = true;
+    };
+    ".config/emacs" = {
+    	source = ./config/emacs;
+	recursive = true;
     };
   };
 
